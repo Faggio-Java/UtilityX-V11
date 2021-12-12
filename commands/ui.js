98157340c58f86b20382f7e6ef0353c23a696af6
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2021-12-12 15:40:50 -0600

    UI CMD ADD

--- commands/ui.js
+++ commands/ui.js
@@ -1,1 +1,19 @@
+const Discord = require("discord.js");
+const moment = require("moment")
+require("moment-duration-format")
 
+module.exports.run = async (client, message, args) => {
+
+let user = message.mentions.users.first() || message.author;
+    let member = message.mentions.members.first() || message.member;
+
+const embed = new Discord.MessageEmbed()
+.setTitle(`${member.displayName}'s User Info`)
+.setDescription(`Username: ${user.username} \n Nickname: ${member.displayName} \n ID: ${user.id} \n Created: ${moment.utc(member.user.createdAt).format("dddd, MMMM Do YYYY, HH:mm")} \n Joined: ${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm")}`);
+message.channel.send({embeds: [embed]})
+
+} 
+                module.exports.config = {
+                    name: 'ui',
+                    aliases: [],
+                };
