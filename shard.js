d584551967c2f11752ea46eedd4b78edb52a709d
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2021-12-12 15:45:49 -0600

    Sharding file deleted
    it was deleted because outdated and since new handler was introduced performance is no longer a problem

--- shard.js
+++
@@ -1,5 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./index.js', { token: 'Token' });
-
-manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
-manager.spawn(35, 9000);
